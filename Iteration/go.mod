200ef56802e5edd9ba73fc0ba35a21d170e0b974
asasmith <smith.asa.la@gmail.com>
2023-12-10 08:50:14 -0500

    iteration

---
+++ Iteration/go.mod
@@ -0,0 +1,9 @@
+module learn-go-with-tdd.com/Iteration
+
+go 1.21.4
+
+require (
+	github.com/yuin/goldmark v1.4.13 // indirect
+	golang.org/x/mod v0.14.0 // indirect
+	golang.org/x/tools v0.16.0 // indirect
+)
---
+++ Iteration/repeat_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 3)
+	}
+}
+
+func ExampleRepeat() {
+	repeatedVal := Repeat("a", 3)
+
+	fmt.Println(repeatedVal)
+
+	// Output: aaa
+}
